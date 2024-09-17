5103dcf1e5c586aa97d428101e8145cd7d4a2210
Roma <nikitinroma2605@gmail.com>
2024-09-17 12:23:53 +0300

    updated to handle error cases

--- data-safety/Utils/LinearCongruentialGenerator.cs
+++ data-safety/Utils/LinearCongruentialGenerator.cs
@@ -13,6 +13,8 @@ public class LinearCongruentialGenerator
     {
         BigInteger Xn = X0;
         BigInteger prev;
+        BigInteger X1 = -1;
+
         var randomNumbersSequence = new List<string>() {X0.ToString()};
         int period = 0;
         
@@ -21,22 +23,30 @@ public class LinearCongruentialGenerator
         const int batchSize = 1_000_000;
         randomNumbersSequenceToBeWritten.Append(X0.ToString());
         
-        while (true) {
+        while (true)
+        {
             prev = Xn;
             Xn = (a * Xn + c) % m;
             period++;
             
-            if (Xn == prev || Xn == X0)
+            // Store the second number if not already stored
+            if (period == 1)
+            {
+                X1 = Xn;
+            }
+            
+            if (Xn == prev || Xn == X0 || (period > 1 && Xn == X1))
             {
                 break;  // Break the loop if a cycle is detected
             }
+            
             
             if (randomNumbersSequence.Count < sequenceLength)
             {
                 randomNumbersSequence.Add(Xn.ToString());
             }
             
-            randomNumbersSequenceToBeWritten.Append(", " + Xn.ToString());
+            randomNumbersSequenceToBeWritten.Append(", " + Xn);
             if (period % batchSize == 0)
             {
                 await writer.WriteAsync(randomNumbersSequenceToBeWritten.ToString());
